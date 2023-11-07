4955534788c42f047bbd4a29d88c0436e34f3d40
sikandaral <42680181+sikandaral@users.noreply.github.com>
2023-11-07 22:03:56 +0500

    bark implement

--- backend/server.py
+++ backend/server.py
@@ -3,16 +3,21 @@ from gtts import gTTS
 import os
 from flask_cors import CORS, cross_origin
 import pyttsx3
-
+import replicate
+from os import environ
+from getpass import getpass
+# import REPLICATE_API_TOKEN
 
 app = Flask(__name__)
 cors = CORS(app)
 
-# MAle id 0,7,11,32
-# Female id10,28,33,38,40,41
+
+REPLICATE_API_TOKEN = 'r8_K1cFQOLKUuw9nzTzc6HeRWHbtDkRSFP0JNUHl'
+environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN
 
 
-# Route for receiving text
+
+# # Route for receiving text
 @app.route('/',methods=['POST', 'GET'])
 @cross_origin()
 def receive_text():
@@ -21,43 +26,67 @@ def receive_text():
     # Getting the text from the request
     if request.method == 'POST':
         req = request.json['text']
-        pitch = float(request.json.get('pitch', 0.5))
-        rate = int(request.json.get('rate', 150))
-        gender = request.json.get('gender', 'male1') 
+        wave_temp = float(request.json.get('pitch', 0.5))
+        text_temp = float(request.json.get('rate', 0.5))
+        gender = request.json.get('gender', 'announcer') 
         print(req)
-        print("Pitch:",  pitch)
-        print("Rate:", rate)
+        print("wave_temp:",  wave_temp)
+        print("text_temp:", text_temp)
+        print("Speaker:", gender)
 
         
 
         try:
 
-            engine = pyttsx3.init()
-            engine.setProperty('voice', 'en-us')
-            voices = engine.getProperty('voices')
+            output = replicate.run(
+            "suno-ai/bark:b76242b40d67c76ab6742e987628a2a9ac019e11d56ab96c4e91ce03b79b2787",
+            input={ "prompt": req,
+                   "history_prompt": gender,
+                   "sample_rate":4 ,
+                   "text_temp":text_temp,
+                   "wave_temp":wave_temp,
+                }
+            )
+            print(output)
+            return output['audio_out']
 
-            if gender == 'male1':
-                engine.setProperty("voice", voices[0].id)
-            elif gender == 'male2':
-                engine.setProperty("voice", voices[11].id)
-            elif gender == 'female1':
-                engine.setProperty("voice", voices[41].id)
-            elif gender == 'female2':
-                engine.setProperty("voice", voices[10].id)
+            # audio_link = output['audio_out']
+
+
+            # audio_file = "output.mp3"
+            # audio_output.save(os.path.join(os.path.dirname(__file__), audio_file))
+            # return send_file(audio_file, mimetype="audio/mpeg",as_attachment=True)
+        
+
             
 
-            engine.setProperty("pitch", pitch)
-            engine.setProperty('rate', rate)
 
-            audio_file = "output.mp3"
+            # engine = pyttsx3.init()
+            # engine.setProperty('voice', 'en-us')
+            # voices = engine.getProperty('voices')
 
-            engine.save_to_file(req , os.path.join(os.path.dirname(__file__), audio_file))
-            engine.runAndWait()
+            # if gender == 'male1':
+            #     engine.setProperty("voice", voices[0].id)
+            # elif gender == 'male2':
+            #     engine.setProperty("voice", voices[11].id)
+            # elif gender == 'female1':
+            #     engine.setProperty("voice", voices[41].id)
+            # elif gender == 'female2':
+            #     engine.setProperty("voice", voices[10].id)
+            
+
+            # engine.setProperty("pitch", pitch)
+            # engine.setProperty('rate', rate)
+
+            # audio_file = "output.mp3"
+
+            # engine.save_to_file(req , os.path.join(os.path.dirname(__file__), audio_file))
+            # engine.runAndWait()
           
             # audio_output = gTTS(req)
             
             # audio_output.save(os.path.join(os.path.dirname(__file__), audio_file))
-            return send_file(audio_file, mimetype="audio/mpeg",as_attachment=True)
+            # return send_file(audio_file, mimetype="audio/mpeg",as_attachment=True)
         
 
         
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   state = {
     text: "",
     pitch: 0.5,
-    rate: 1.0,
-    gender: "neutral",  // Default gender selection
+    rate: 0.5,
+    gender: "announcer", // Default gender selection
     src: "",
     audioKey: 0,
   };
@@ -45,19 +45,18 @@ class App extends Component {
 
       const response = await axios.post(
         "/",
-        { text, pitch, rate, gender },  // Send pitch, rate, and gender values to the backend
+        { text, pitch, rate, gender },
         {
           headers: {
             "Content-Type": "application/json",
           },
-          responseType: "blob",
         }
       );
-      const blob = new Blob([response.data], { type: "audio/mpeg" });
-      const url = URL.createObjectURL(blob);
+
+      const audioLink = response.data;
 
       this.setState((prevState) => ({
-        src: url,
+        src: audioLink,
         audioKey: prevState.audioKey + 1,
       }));
     } catch (error) {
@@ -68,6 +67,142 @@ class App extends Component {
   render() {
     const { text, pitch, rate, gender, src, audioKey } = this.state;
     const maxLength = 30;
+
+    // Define the gender options
+    const genderOptions = [
+      "announcer",
+      "de_speaker_0",
+      "de_speaker_1",
+      "de_speaker_2",
+      "de_speaker_3",
+      "de_speaker_4",
+      "de_speaker_5",
+      "de_speaker_6",
+      "de_speaker_7",
+      "de_speaker_8",
+      "de_speaker_9",
+      "en_speaker_0",
+      "en_speaker_1",
+      "en_speaker_2",
+      "en_speaker_3",
+      "en_speaker_4",
+      "en_speaker_5",
+      "en_speaker_6",
+      "en_speaker_7",
+      "en_speaker_8",
+      "en_speaker_9",
+      "es_speaker_0",
+      "es_speaker_1",
+      "es_speaker_2",
+      "es_speaker_3",
+      "es_speaker_4",
+      "es_speaker_5",
+      "es_speaker_6",
+      "es_speaker_7",
+      "es_speaker_8",
+      "es_speaker_9",
+      "fr_speaker_0",
+      "fr_speaker_1",
+      "fr_speaker_2",
+      "fr_speaker_3",
+      "fr_speaker_4",
+      "fr_speaker_5",
+      "fr_speaker_6",
+      "fr_speaker_7",
+      "fr_speaker_8",
+      "fr_speaker_9",
+      "hi_speaker_0",
+      "hi_speaker_1",
+      "hi_speaker_2",
+      "hi_speaker_3",
+      "hi_speaker_4",
+      "hi_speaker_5",
+      "hi_speaker_6",
+      "hi_speaker_7",
+      "hi_speaker_8",
+      "hi_speaker_9",
+      "it_speaker_0",
+      "it_speaker_1",
+      "it_speaker_2",
+      "it_speaker_3",
+      "it_speaker_4",
+      "it_speaker_5",
+      "it_speaker_6",
+      "it_speaker_7",
+      "it_speaker_8",
+      "it_speaker_9",
+      "ja_speaker_0",
+      "ja_speaker_1",
+      "ja_speaker_2",
+      "ja_speaker_3",
+      "ja_speaker_4",
+      "ja_speaker_5",
+      "ja_speaker_6",
+      "ja_speaker_7",
+      "ja_speaker_8",
+      "ja_speaker_9",
+      "ko_speaker_0",
+      "ko_speaker_1",
+      "ko_speaker_2",
+      "ko_speaker_3",
+      "ko_speaker_4",
+      "ko_speaker_5",
+      "ko_speaker_6",
+      "ko_speaker_7",
+      "ko_speaker_8",
+      "ko_speaker_9",
+      "pl_speaker_0",
+      "pl_speaker_1",
+      "pl_speaker_2",
+      "pl_speaker_3",
+      "pl_speaker_4",
+      "pl_speaker_5",
+      "pl_speaker_6",
+      "pl_speaker_7",
+      "pl_speaker_8",
+      "pl_speaker_9",
+      "pt_speaker_0",
+      "pt_speaker_1",
+      "pt_speaker_2",
+      "pt_speaker_3",
+      "pt_speaker_4",
+      "pt_speaker_5",
+      "pt_speaker_6",
+      "pt_speaker_7",
+      "pt_speaker_8",
+      "pt_speaker_9",
+      "ru_speaker_0",
+      "ru_speaker_1",
+      "ru_speaker_2",
+      "ru_speaker_3",
+      "ru_speaker_4",
+      "ru_speaker_5",
+      "ru_speaker_6",
+      "ru_speaker_7",
+      "ru_speaker_8",
+      "ru_speaker_9",
+      "tr_speaker_0",
+      "tr_speaker_1",
+      "tr_speaker_2",
+      "tr_speaker_3",
+      "tr_speaker_4",
+      "tr_speaker_5",
+      "tr_speaker_6",
+      "tr_speaker_7",
+      "tr_speaker_8",
+      "tr_speaker_9",
+      "zh_speaker_0",
+      "zh_speaker_1",
+      "zh_speaker_2",
+      "zh_speaker_3",
+      "zh_speaker_4",
+      "zh_speaker_5",
+      "zh_speaker_6",
+      "zh_speaker_7",
+      "zh_speaker_8",
+      "zh_speaker_9",
+    ];
+
     return (
       <div>
         <h1>Simple Textbox</h1>
@@ -83,7 +218,7 @@ class App extends Component {
 
         {/* Pitch input */}
         <label>
-          Pitch (0 to 1):
+        waveform_temp (0 to 1):
           <input
             type="number"
             step="0.01"
@@ -96,26 +231,26 @@ class App extends Component {
 
         {/* Rate input */}
         <label>
-          Rate (1 to 1000):
+          text_temp (0 to 1):
           <input
             type="number"
             step="0.1"
-            min="0.1"
-            max="10"
+            min="0"
+            max="1"
             value={rate}
             onChange={this.handleRateChange}
           />
         </label>
 
-        {/* Gender selection */}
+        {/* Gender selection dropdown */}
         <label>
-          Select Gender:
+          Select Speaker:
           <select value={gender} onChange={this.handleGenderChange}>
-        
-            <option value="male1">Male-1</option>
-            <option value="male2">Male-2</option>
-            <option value="female1">Female-1</option>
-            <option value="female2">Female-2</option>
+            {genderOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
           </select>
         </label>
 
